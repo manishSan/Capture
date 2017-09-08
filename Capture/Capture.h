e0433781e489ce4fa29a70423150b975b4e0afb2
Sanwal, Manish <Manish.Sanwal@gmail.com>
2017-09-08 13:26:04 -0400

    Initial Commit

---
+++ Capture/Capture.h
@@ -0,0 +1,19 @@
+//
+//  Capture.h
+//  Capture
+//
+//  Created by Sanwal, Manish on 9/8/17.
+//  Copyright © 2017 Toys R Us. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Capture.
+FOUNDATION_EXPORT double CaptureVersionNumber;
+
+//! Project version string for Capture.
+FOUNDATION_EXPORT const unsigned char CaptureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Capture/PublicHeader.h>
+
+
---
+++ CaptureTests/CaptureTests.swift
@@ -0,0 +1,36 @@
+//
+//  CaptureTests.swift
+//  CaptureTests
+//
+//  Created by Sanwal, Manish on 9/8/17.
+//  Copyright © 2017 Toys R Us. All rights reserved.
+//
+
+import XCTest
+@testable import Capture
+
+class CaptureTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
