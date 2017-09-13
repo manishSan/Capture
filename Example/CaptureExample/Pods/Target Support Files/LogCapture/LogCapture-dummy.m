7bc1de4f6c2d35db3d4068ebfcb6101b548dd327
Sanwal, Manish <Manish.Sanwal@gmail.com>
2017-09-13 10:03:59 -0400

    Removed pod checking for Example folder.

--- Example/CaptureExample/Pods/Target Support Files/LogCapture/LogCapture-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_LogCapture : NSObject
-@end
-@implementation PodsDummy_LogCapture
-@end
--- Example/CaptureExample/Pods/Target Support Files/Pods-CaptureExample/Pods-CaptureExample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CaptureExample : NSObject
-@end
-@implementation PodsDummy_Pods_CaptureExample
-@end
